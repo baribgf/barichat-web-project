3a99bbccf3fcf48abd1d7e9619b67f931fab7f37
bari <96777478+baribgf@users.noreply.github.com>
2022-11-14 00:18:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send("Hello, World! with BARI BGF [WITH NEW CONFIG]")
+  res.send("Hello, World! with BARI BGF")
 })
 
 app.listen(port, () => {console.log(`Server running on port: ${port}`)})
