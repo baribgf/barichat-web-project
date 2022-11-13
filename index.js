d7a047ad46ebe54c32911af6870256a0ed5419c4
bari <96777478+baribgf@users.noreply.github.com>
2022-11-14 00:11:10 +0100

    Update and rename Server.js to index.js

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ const port = process.env.PORT || 3001;
 app.get('/', (req, res) => {
   res.send("Hello, World! with BARI BGF [WITH NEW CONFIG]")
 })
-app.listen(port, () => console.log(`Server running on port: ${port}`))
+
+app.listen(port, () => {console.log(`Server running on port: ${port}`)})
