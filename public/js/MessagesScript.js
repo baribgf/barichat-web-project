390e42fbd695bb84fb098e6bdde6659fc1000473
bari <96777478+baribgf@users.noreply.github.com>
2022-11-14 23:33:56 +0100

    Update MessagesScript.js

--- public/js/MessagesScript.js
+++ public/js/MessagesScript.js
@@ -129,7 +129,7 @@ function LoadChat() {
 
 setInterval(async ()=>{
     if (PairUserID != null) await LoadChat();
-}, 100);
+}, 500);
 
 SendButton.onclick = () => {
     let sentMessage = `\n${UserID}:${MessageText.value.replaceAll('\n', "\\n")}`;
